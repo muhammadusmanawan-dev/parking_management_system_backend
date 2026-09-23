753eb29c6a050cee6b75495542b5fff4d21cfcf6
Muhammad Usman Awan <muhammad.usman@qbatch.com>
2026-09-24 02:06:40 +0500

    FIXED: Fixed issues realted to db and added admin interface for admin and fixed ticket serializer

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Ticket
 
-# Register your models here.
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'parking_spot', 'status', 'entry_time')
+
+    list_filter = ('status', 'entry_time')
+    
+    search_fields = ('id', 'parking_spot__spot_number')
