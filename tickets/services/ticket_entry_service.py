49bb9bf8f1b21f2d9f8598cf370c00f113eb803f
Muhammad Usman Awan <muhammad.usman@qbatch.com>
2026-09-23 19:44:56 +0500

    Add ticket service files

---
+++ tickets/services/ticket_entry_service.py
@@ -0,0 +1,62 @@
+from django.db import transaction
+
+from customers.models import Customer
+from parking_spots.models import ParkingSpot, ParkingSpotStatus
+from vehicles.models import Vehicle
+
+from ..serializers import TicketSerializer
+from .ticket_service import TicketService
+
+
+class TicketEntryService:
+
+    @staticmethod
+    @transaction.atomic
+    def create_entry(name,phone_number,license_plate,vehicle_type,):
+        customer, customer_created = Customer.objects.get_or_create(
+            phone_number=phone_number,
+            defaults={
+                "name": name,
+            },
+        )
+
+        vehicle, vehicle_created = Vehicle.objects.get_or_create(
+            license_plate=license_plate,
+            defaults={
+                "vehicle_type": vehicle_type,
+            },
+        )
+
+        if not vehicle_created and vehicle.vehicle_type != vehicle_type:
+            raise ValueError(
+                "Vehicle type does not match the registered vehicle."
+            )
+
+        #for POSTgreSQL
+        #parking_spot = ParkingSpot.objects.select_for_update().filter(spot_type=vehicle_type,status=ParkingSpotStatus.AVAILABLE,).first()
+        # if parking_spot is None: 
+        #     raise ValueError( "No parking spot available." )
+        
+
+        #for db.sqlite3
+        while True:
+            parking_spot = ParkingSpot.objects.filter( spot_type=vehicle_type, status=ParkingSpotStatus.AVAILABLE, ).first() 
+            if parking_spot is None: 
+                raise ValueError( "No parking spot available." )
+            
+            updated = ParkingSpot.objects.filter( id=parking_spot.id, status=ParkingSpotStatus.AVAILABLE, ).update( status=ParkingSpotStatus.OCCUPIED ) 
+            if updated == 1:
+                break
+
+
+    
+        ticket_serializer = TicketSerializer(data={"customer":customer.id, "vehicle": vehicle.id,"parking_spot": parking_spot.id,})
+        ticket_serializer.is_valid(raise_exception=True)
+        ticket = TicketService.create_ticket(ticket_serializer)
+        parking_spot.refresh_from_db()
+        return{
+            "customer": customer,
+            "vehicle": vehicle,
+            "parking_spot": parking_spot,
+            "ticket": ticket,
+        }
